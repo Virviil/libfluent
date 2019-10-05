c53e8677989f9462a8604ddde5808601fcd6f6e6
Dmitry Rubinstein <rubinsteindb@gmail.com>
2019-10-05 16:55:42 +0300

    Fixing tests

--- README.md
+++ README.md
@@ -126,4 +126,4 @@ end
 
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/fluent](https://hexdocs.pm/libfluent).
+be found at [https://hexdocs.pm/libfluent](https://hexdocs.pm/libfluent).
--- test/support/test_assemblies.ex
+++ test/support/test_assemblies.ex
@@ -1,11 +1,14 @@
 defmodule Assembly.Empty do
+  @moduledoc false
   use Fluent.Assembly, otp_app: :libfluent
 end
 
 defmodule Assembly.WithPriv do
+  @moduledoc false
   use Fluent.Assembly, otp_app: :libfluent, priv: "another-priv-folder"
 end
 
 defmodule Assembly.DefaultLanguageChanged do
+  @moduledoc false
   use Fluent.Assembly, otp_app: :libfluent, priv: "another-priv-folder"
 end
