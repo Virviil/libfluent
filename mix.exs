b1b79f63851b03df078952d1996bec977b68a6d9
Dmitry Rubinstein <rubinsteindb@gmail.com>
2021-10-10 00:19:29 +0300

    Update elixir libs

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Fluent.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.22.0"},
+      {:rustler, "~> 0.22"},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:excoveralls, "~> 0.13", only: :test},
